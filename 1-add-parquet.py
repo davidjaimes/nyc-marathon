12c0e7931c5b0d4acf20010ec31cc3ffad6a7cee
David Jaimes <david@djaimes.com>
2019-04-04 08:59:18 -0700

    add 2018 parquet files

---
+++ 1-add-parquet.py
@@ -0,0 +1,16 @@
+import glob as gl
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+fnames = gl.glob("*.parquet")
+fnames = sorted(fnames)
+for i, f in enumerate(fnames):
+    t = pq.read_table(f)
+    if i == 0:
+        df1 = t.to_pandas()
+    else:
+        df1 = pd.concat([df1, t.to_pandas()])
+df = df1.reset_index().drop(columns=['index'])
+table = pa.Table.from_pandas(df)
+pq.write_table(table, 'nyc-marathon-2018-eventRunner.parquet')
