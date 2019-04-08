ac8072ca390b0132dbecccfcf8711eb45f8c394c
David Jaimes <david@djaimes.com>
2019-04-08 11:47:00 -0700

    combine splits

---
+++ 3-add-split.py
@@ -0,0 +1,16 @@
+import glob as gl
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+fnames = gl.glob("*.csv")
+fnames = sorted(fnames)
+for i, f in enumerate(fnames):
+    df = pd.read_csv(f)
+    if i == 0:
+        df1 = df
+    else:
+        df1 = pd.concat([df1, df])
+df = df1.reset_index().drop(columns=['index', 'Unnamed: 0'])
+table = pa.Table.from_pandas(df)
+pq.write_table(table, 'nyc-marathon-2018-resultDetails.parquet')
