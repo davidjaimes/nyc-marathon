cdc76533c1c95bbe82efacc495c4875acafac6aa
David Jaimes <david@djaimes.com>
2019-05-21 22:05:53 -0700

    get 20,000 bibs from 2002

--- 2-get-split.py
+++ 2-get-split.py
@@ -4,6 +4,8 @@ import pyarrow.parquet as pq
 import requests as rq
 from tqdm import tqdm
 
+#token = 'ebe04e9c08064538'  #years 2004-2018
+token = '6112c32703f442f0'
 url = 'https://results.nyrr.org/api/runners/resultDetails'
 h = {
     'origin': 'https://results.nyrr.org',
@@ -15,12 +17,12 @@ h = {
     'referer': 'https://results.nyrr.org/',
     'authority': 'results.nyrr.org',
     'cookie': '__cfduid=d1123b77692189442e29a6ae78e6d17391554342089; _gcl_au=1.1.1614746071.1554342091; _ga=GA1.2.1285202861.1554342092; _gid=GA1.2.887065544.1554342092; _fbp=fb.1.1554342092334.1341848325; ARRAffinity=da4c4ff244aae03ae3c7548f243f7b2b5c22567a56a76a62aaebcf4acc7f0bf8; _ga=GA1.3.1285202861.1554342092; _gid=GA1.3.887065544.1554342092; __atuvc=2%7C14; __atuvs=5ca560d54f9be40a001',
-    'token': 'ebe04e9c08064538',
+    'token': f'{token}',
 }
-number = 4
+number = 3
 start = int(f'{number - 1}0000')
 end = int(f'{number}0000')
-t = pq.read_table('parquet/nyc-marathon-2004-eventRunner.parquet')
+t = pq.read_table('parquet/nyc-marathon-2003-eventRunner.parquet')
 df = t.to_pandas()
 for i, id in enumerate(tqdm(df['runnerId'][start:end])):
     d = f'{{"runnerId":{id}}}'
