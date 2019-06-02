a435fe377698ae9e7ec350f9b112c25db3d03dfa
David Jaimes <david@djaimes.com>
2019-06-02 11:14:13 -0700

    finish with 1997 results

--- 2-get-split.py
+++ 2-get-split.py
@@ -22,7 +22,7 @@ h = {
 number = 2
 start = int(f'{number - 1}0000')
 end = int(f'{number}0000')
-t = pq.read_table('parquet/nyc-marathon-1998-eventRunner.parquet')
+t = pq.read_table('parquet/nyc-marathon-1997-eventRunner.parquet')
 df = t.to_pandas()
 for i, id in enumerate(tqdm(df['runnerId'][start:end])):
     d = f'{{"runnerId":{id}}}'
