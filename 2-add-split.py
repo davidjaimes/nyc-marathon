5a33f55267d321984d1205d38e24494753491705
davidjaimes <david@djaimes.com>
2019-06-21 10:55:05 -0700

    finish 1983 results

--- 2-add-split.py
+++ 2-add-split.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-year = 1982
+year = 1983
 fnames = gl.glob("*.csv")
 fnames = sorted(fnames)
 for i, f in enumerate(fnames):
