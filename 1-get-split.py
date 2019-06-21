64b3435fccdbfa1d8b7a6ee59120bcd899f67121
davidjaimes <david@djaimes.com>
2019-06-21 14:17:02 -0700

    finish 1988 results

--- 1-get-split.py
+++ 1-get-split.py
@@ -20,7 +20,7 @@ h = {
     'token': f'{token}',
 }
 number = 3
-year = 1987
+year = 1988
 start = int(f'{number - 1}0000')
 end = int(f'{number}0000')
 t = pq.read_table(f'eventRunner/nyc-marathon-{year}-eventRunner.parquet')
