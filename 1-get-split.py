32f405974d0042cafd0eb758f2a720e2a9240eca
davidjaimes <david@djaimes.com>
2019-06-24 12:06:09 -0700

    finish 2004 results

--- 1-get-split.py
+++ 1-get-split.py
@@ -19,7 +19,7 @@ h = {
     'cookie': '__cfduid=d1123b77692189442e29a6ae78e6d17391554342089; _gcl_au=1.1.1614746071.1554342091; _ga=GA1.2.1285202861.1554342092; _gid=GA1.2.887065544.1554342092; _fbp=fb.1.1554342092334.1341848325; ARRAffinity=da4c4ff244aae03ae3c7548f243f7b2b5c22567a56a76a62aaebcf4acc7f0bf8; _ga=GA1.3.1285202861.1554342092; _gid=GA1.3.887065544.1554342092; __atuvc=2%7C14; __atuvs=5ca560d54f9be40a001',
     'token': f'{token}',
 }
-number = 1
+number = 4
 year = 2004
 start = int(f'{number - 1}0000')
 end = int(f'{number}0000')
--- 2-add-split.py
+++ 2-add-split.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-year = 2003
+year = 2004
 fnames = gl.glob("*.csv")
 fnames = sorted(fnames)
 for i, f in enumerate(fnames):
